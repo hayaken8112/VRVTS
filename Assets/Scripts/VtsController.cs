216dd0cbe75726ad658d974e826dff79df607635
hayaken8112 <hayashi8112@gmail.com>
2018-11-20 11:05:50 +0900

    create artwork data class

--- Assets/Scripts/VtsController.cs
+++ Assets/Scripts/VtsController.cs
@@ -14,40 +14,26 @@ public class VtsController : MonoBehaviour {
 	int imgIndex = 0;
 	public int ImgId = 10;
 	public List<Sprite> images;
-	public List<Dictionary<string,int>> sizes;
+	public List<ArtworkData> artworks;
 	[SerializeField]
 	CommentManager commentManager;
-	Dictionary<string,int> size0 = new Dictionary<string,int>(){
-		{"width", 180},
-		{"height", 250},
-		{"Id", 10}
-	};
-	Dictionary<string,int> size1 = new Dictionary<string,int>(){
-		{"width", 600},
-		{"height", 400},
-		{"Id", 9}
-	};
-	Dictionary<string,int> size2 = new Dictionary<string,int>(){
-		{"width", 200},
-		{"height", 300},
-		{"Id", 8}
-	};
+	ArtworkData art0 = new ArtworkData(180,250,10);
+	ArtworkData art1 = new ArtworkData(600,400,9);
+	ArtworkData art2 = new ArtworkData(200,300,8);
 
 
 	// Use this for initialization
 	void Start () {
 		RectTransform artworkRect = artworkObj.GetComponent<RectTransform>();		
 		Image artworkImg = artworkObj.GetComponent<Image>();
-		sizes = new List<Dictionary<string,int>>(){
-			size0,size1,size2
-		};
+		artworks = new List<ArtworkData>(){art0, art1, art2};
 		gridView = artworkObj.GetComponentInChildren<GridView>();
 		nextButton.OnClickAsObservable().Subscribe(_ => {
 			imgIndex = (imgIndex + 1) % 3;
 			artworkImg.sprite = images[imgIndex];
-			artworkRect.sizeDelta = new Vector2(sizes[imgIndex]["width"],sizes[imgIndex]["height"]);
+			artworkRect.sizeDelta = new Vector2(artworks[imgIndex].width,artworks[imgIndex].height);
 			gridView.InitGrid();
-			commentManager.workId = sizes[imgIndex]["Id"];
+			commentManager.workId = artworks[imgIndex].id;
 		});
 		
 	}
