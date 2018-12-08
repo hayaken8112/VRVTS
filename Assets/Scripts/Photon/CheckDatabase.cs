868e0be2150d8f210567b04b11c03b7a2634b2f9
豊嶋そうや <a.ta19.lo96u713@gmail.com>
2018-12-08 15:20:58 +0900

    database追加 user登録追加

---
+++ Assets/Scripts/Photon/CheckDatabase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CheckDatabase: MonoBehaviour
+{
+	[RuntimeInitializeOnLoadMethod()]
+	static void Init()
+	{
+
+		bool yes;
+		SqliteDatabase sqlDB = new SqliteDatabase("config.db");
+		try {
+			sqlDB.ExecuteQuery("select * from ddd");
+			yes = true;
+		} catch (SqliteException e) {
+			yes = false;
+		}
+
+		if (yes){
+			Debug.Log("このデータベースはありました");
+			SceneManager.LoadScene("New Scene");
+		}else{
+			Debug.Log("このデータベースはありません");
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Photon/RegisterUser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterUser : MonoBehaviour {
+
+	public InputField nameIF;
+
+	public static string userName;
+
+	public void Regsiter(){
+
+		Debug.Log("押された！");
+
+		userName = nameIF.text;
+
+		if (userName == ""){
+			Debug.Log("名前が入力されていません");
+			return;
+		}
+
+		SqliteDatabase sqlDB = new SqliteDatabase("config.db");
+		string query = "create table ddd(name integer, age integer)";
+        sqlDB.ExecuteNonQuery(query);
+
+		string querysecond = "insert into ddd values('" + userName + "', 19)";
+        sqlDB.ExecuteNonQuery(querysecond);
+	}
+}
+
